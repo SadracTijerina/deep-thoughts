89358d2445e2e7e20c03cc516d9873c0fb5f1aa1
SadracTijerina <reavity3@gmail.com>
2021-10-10 16:05:36 -0500

    finished server side

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+const { authMiddleware } = require("./utils/auth");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+// integrate our Apollo server with the Express application as middleware
+async function startApolloServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+}
+startApolloServer();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
