c3fbb0f3e4885502333825c005c0eb68212ae2d9
SadracTijerina <reavity3@gmail.com>
2021-10-14 00:17:23 -0500

    added reactions

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { useQuery } from "@apollo/client";
 
-import { QUERY_THOUGHT } from "../utils/queries";
 import ReactionList from "../components/ReactionList";
+import ReactionForm from "../components/ReactionForm";
+
+import Auth from "../utils/auth";
+import { useQuery } from "@apollo/client";
+import { QUERY_THOUGHT } from "../utils/queries";
 
 const SingleThought = (props) => {
   const { id: thoughtId } = useParams();
-  console.log(thoughtId);
 
   const { loading, data } = useQuery(QUERY_THOUGHT, {
     variables: { id: thoughtId },
@@ -36,6 +38,8 @@ const SingleThought = (props) => {
       {thought.reactionCount > 0 && (
         <ReactionList reactions={thought.reactions} />
       )}
+
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
